c9a05c34df0e9b63c12bfc12626b276544c94c96
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-04-18 13:06:40 +0530

    Added logout controller

--- api/controllers/authController.js
+++ api/controllers/authController.js
@@ -75,13 +75,21 @@ export const login = async (req, res) => {
       });
     }
 
-    console.log(await user.matchPassword(password));
-
     await setAuthCookie(user, res);
 
     res.json({ success: true, user });
   } catch (err) {
-    console.log('Error in signup controller: ', err);
+    console.log('Error in login controller: ', err);
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
+
+export const logout = async (req, res) => {
+  try {
+    res.clearCookie('token');
+    res.status(200).json({ success: true, message: 'Logged out successfully' });
+  } catch (err) {
+    console.log('Error in logout controller: ', err);
     res.status(500).json({ success: false, message: 'Server error' });
   }
 };
