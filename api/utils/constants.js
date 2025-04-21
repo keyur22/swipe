4974c16dd1893d084b508952fca2a4fa5c17f0b8
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-04-21 13:50:59 +0530

    Updated server and db validations for user

---
+++ api/utils/constants.js
@@ -0,0 +1,11 @@
+export const USER_SAFE_FIELDS = [
+  'name',
+  'email',
+  'age',
+  'gender',
+  'genderPreference',
+  'about'
+];
+
+export const allowedGenders = ['male', 'female'];
+export const allowedGenderPreference = ['male', 'female', 'others'];
