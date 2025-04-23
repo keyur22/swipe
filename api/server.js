ca8c9d30a8979eb45a063edc954b49dc951e039d
Keyur Chaudhari <keyurchaudhari4@gmail.com>
2025-04-23 14:09:22 +0530

    Deployment setup

--- api/server.js
+++ api/server.js
@@ -11,12 +11,15 @@ import userRoutes from './routes/userRoutes.js';
 
 import connectDb from './config/database.js';
 import { initializeSocket } from './socket/socket.server.js';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.port || 5000;
 const httpServer = createServer(app);
+
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '200mb' }));
 app.use(cookieParser());
@@ -33,6 +36,14 @@ app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/matches', matchRoutes);
 app.use('/api/messages', messageRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
+  });
+}
 
 httpServer.listen(PORT, () => {
   console.log('Server started at this port:  ' + PORT);
